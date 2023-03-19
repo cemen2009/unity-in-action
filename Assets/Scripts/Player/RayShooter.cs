3e72efb6ed360d175e549c77f5d8ded15d0b9626
onopriy <98355726+cemen2009@users.noreply.github.com>
2023-03-19 15:55:27 +0200

    cursor + sphere on position of hit

--- Assets/Scripts/Player/RayShooter.cs
+++ Assets/Scripts/Player/RayShooter.cs
@@ -6,11 +6,16 @@ public class RayShooter : MonoBehaviour
 {
     private Camera _camera;
     private Vector3 point;
+    private int size = 12;
+
     // Start is called before the first frame update
     void Start()
     {
         _camera = GetComponent<Camera>();
         point = new Vector3(_camera.pixelWidth / 2, _camera.pixelHeight /  2, 0);   // point = center of screen
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = true;
     }
 
     // Update is called once per frame
@@ -21,8 +26,28 @@ public class RayShooter : MonoBehaviour
             Ray ray = _camera.ScreenPointToRay(point);  // drop ray from point(center of screen)
             if (Physics.Raycast(ray, out RaycastHit hit))   // hit is struct of data about ray, where is hit
             {
-                Debug.Log("Hit " + hit.point);
+                StartCoroutine(SphereIndicator(hit.point));
             }
         }
+    }
+
+    private IEnumerator SphereIndicator(Vector3 position)
+    {
+        // create sphere and give it position of hit
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.transform.position = position;
+        
+        // sphere exists one second
+        yield return new WaitForSeconds(1);
+
+        // and destroys
+        Destroy(sphere);
+    }
+
+    private void OnGUI()
+    {
+        float posX = _camera.pixelWidth / 2 - size / 4;
+        float posY = _camera.pixelHeight / 2 - size / 2;
+        GUI.Label(new Rect(posX, posY, size, size), "+");
     }
 }
